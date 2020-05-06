d2b447b49e85d56813eec9f0f5d98679cb34b9ac
d0561910 <d0561910@mail.fcu.edu.tw>
2020-05-06 23:14:30 +0800

    test: add login api test case reason: after finish an api need write test case changing: ./__test__/sample.test.js issue: #3

---
+++ server/__test__/sample.test.js
@@ -0,0 +1,73 @@
+import request from "supertest";
+import "regenerator-runtime/runtime";
+import server from "../bin/www";
+import admin from "../config/firebase.config";
+
+// @Test '/' route
+describe("Get Endpoints", () => {
+  it("Get", async (done) => {
+    const res = await request(server).get("/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("nome");
+    done();
+  });
+});
+
+// @Test '/main' route
+describe("Get Endpoints", () => {
+  it("Get", async (done) => {
+    const res = await request(server).get("/main");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("title");
+    done();
+  });
+});
+
+// @Test '/users' route
+describe("Get Endpoints", () => {
+  it("Get", async (done) => {
+    const res = await request(server).get("/users");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("msg");
+    done();
+  });
+});
+
+// @Test '/login' route with true username and password;
+describe("Post login api", () => {
+  it("Post", async (done) => {
+    const res = await request(server)
+      .post("/api/login")
+      .send({ name: "test", password: "123456" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("token");
+    done();
+  });
+});
+
+// @Test '/login' route with wrong username and password;
+describe("Post login api", () => {
+  it("Post", async (done) => {
+    const res = await request(server)
+      .post("/api/login")
+      .send({ name: "tester", password: "6543211" });
+    expect(res.statusCode).toEqual(403);
+    expect(res.body).toHaveProperty("error");
+    done();
+  });
+});
+
+// @Test unknow route
+describe("Get Endpoints", () => {
+  it("Get", async (done) => {
+    const res = await request(server).get("/mains").send();
+    expect(res.statusCode).toEqual(404);
+    done();
+  });
+});
+
+afterAll(async (done) => {
+  // close server conection
+  server.close();
+  done();
+});
