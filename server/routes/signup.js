0f9207955e9790befac4988ed13cf8584114b207
D0561910 <d0561910@mail.fcu.edu.tw>
2020-05-10 13:40:26 +0800

    fix: remove console.log(). reason: console.log() problem. changing: ./routes/signup.jsissue: #12

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -7,12 +7,11 @@ import schemas from "../utils/schemas";
 const router = express.Router();
 
 router.post("/signup", validation(schemas.signUpSchema), async (req, res) => {
-    
+
     const saltRounds = 10;
     // for register new account
     bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
         // Store hash in your password DB.
-        console.log(`admin ${hash}`);
         admin.database().ref("users/").push({
             email: req.body.email,
             id: Math.ceil(Math.random() * 9999999),
