3b94f6a3d2ccdccecbc191afe84d1644f1a6aa69
D0561910 <D0561910@mail.fcu.edu.tw>
2020-05-14 16:48:49 +0800

    fix: when user register with same email system will throw out error reason: because when user register with same email, the system doesn't throw error changing: ./server/routes/signup.js issue: #19

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -6,22 +6,47 @@ import schemas from "../utils/schemas";
 
 const router = express.Router();
 
+function checkUser(email) {
+    return new Promise((resolve) => {
+        admin
+            .database()
+            .ref("/users/")
+            .once("value")
+            .then((snap) => {
+                var child = snap.val()
+                for (let i in child) {
+                    if (child[i].email == email) {
+                        return resolve("true");
+                    }
+                }
+                return resolve("false");
+            });
+    });
+}
+
 router.post("/signup", validation(schemas.signUpSchema), async (req, res) => {
+
+    const reply = await checkUser(req.body.email);
 
     const saltRounds = 10;
     // for register new account
-    bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
-        // Store hash in your password DB.
-        admin.database().ref("users/").push({
-            email: req.body.email,
-            name: req.body.name,
-            password: hash
+    if (reply == "false") {
+        bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
+            // Store hash in your password DB.
+            admin.database().ref("users/").push({
+                email: req.body.email,
+                name: req.body.name,
+                password: hash
+            });
+            res.json({
+                ret_msg: "Register Successfully"
+            });
         });
+    } else {
         res.json({
-            ret_msg: "注册成功"
+            ret_msg: "E-mail Already Register try another e-mail"
         });
-    });
-
+    }
 });
 
 export default router;
