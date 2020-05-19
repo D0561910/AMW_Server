7e15488a102b8a7375446a7d8f2436534c952c12
d0561910 <d0561910@mail.fcu.edu.tw>
2020-05-19 20:39:02 +0800

    refactor: remove AppError.js reason: Because no use at app.js changing: ./server/utils/AppError.js ./server/app.js issue: #40

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import APPError from "./utils/AppError";
+import helmet from "helmet";
 
 import indexRouter from "./routes/indexRouter";
 import usersRouter from "./routes/usersRouter";
@@ -10,6 +10,15 @@ import signUpRouter from "./routes/signupRouter";
 import projectRouter from "./routes/projectRouter";
 
 var app = express();
+
+app.use(helmet());
+app.use(
+  helmet({
+    frameguard: {
+      action: "deny",
+    },
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -25,32 +34,6 @@ app.use("/api", projectRouter);
 // Route to be tested
 app.get("/", (req, res) => {
   return res.status(200).json({ nome: "Handsome Charles Sin" });
-});
-
-// if error is not an instanceOf APIError, convert it.
-app.use((err, req, res, next) => {
-  let errorMessage;
-  let errorCode;
-  let errorStatus;
-  // express validation error 所有傳入參數驗證錯誤
-  if (err instanceof expressValidation.ValidationError) {
-    if (
-      err.errors[0].location === "query" ||
-      err.errors[0].location === "body"
-    ) {
-      errorMessage = err.errors[0].messages;
-      errorCode = 400;
-      errorStatus = httpStatus.BAD_REQUEST;
-    }
-    const error = new APPError.APIError(
-      errorMessage,
-      errorStatus,
-      true,
-      errorCode
-    );
-    return next(error);
-  }
-  return next(err);
 });
 
 // error handler, send stacktrace only during development 錯誤後最後才跑這邊
