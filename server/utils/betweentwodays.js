4b1f3d54f43540ea1930d82cdd8ed98f3afb1a32
d0561910 <d0561910@mail.fcu.edu.tw>
2020-05-20 12:30:12 +0800

    fix: issue #45 chaging: ./server/utils/betweenTwoDays.js

--- server/utils/betweentwodays.js
+++ server/utils/betweentwodays.js
@@ -5,13 +5,17 @@ const betweenTwoDays = (start_date, end_date, date_format = "MM-DD-YYYY") => {
     const getDateAsArray = (date) => {
         return moment(date.split(/\D+/), date_format);
     };
+
     const diff =
         getDateAsArray(end_date).diff(getDateAsArray(start_date), "days") + 1;
+    
     const dates = [];
+    
     for (let i = 0; i < diff; i++) {
         const nextDate = getDateAsArray(start_date).add(i, "day");
         dates.push(nextDate.format("DD-MMM-YYYY"));
     }
+    
     return dates;
 };
 
