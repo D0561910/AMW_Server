247a50a60d7fd54578e95d99d3715258f50fb307
d0561910 <d0561910@mail.fcu.edu.tw>
2020-05-24 16:09:41 +0800

    feat: add Travis CI reason: 之前給其他人修改了。 changing: ./language: node_js node_js:   - 10 install:   - npm install script:   - npm run clean   - npm run build

--- server/app.js
+++ server/app.js
@@ -23,10 +23,10 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 app.use(limiter);
-app.use("/api/signup", signupLimiter)
+app.use("/api/signup", signupLimiter);
 
 app.use("/api", loginRouter);
 app.use("/api", signUpRouter);
